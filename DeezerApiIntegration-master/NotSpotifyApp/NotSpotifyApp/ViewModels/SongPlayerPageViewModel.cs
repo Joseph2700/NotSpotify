ca8ba5f80852f3f824f85218b3b8b20f94fad666
ssmatos <sebastiansoto.fms@gmail.com>
2020-03-27 14:00:32 -0400

    Song Player View Model

--- DeezerApiIntegration-master/NotSpotifyApp/NotSpotifyApp/ViewModels/SongPlayerPageViewModel.cs
+++ DeezerApiIntegration-master/NotSpotifyApp/NotSpotifyApp/ViewModels/SongPlayerPageViewModel.cs
@@ -1,10 +1,62 @@
-﻿using System;
+﻿using NotSpotifyApp.Models;
+using NotSpotifyApp.Services;
+using Plugin.Share;
+using Plugin.Share.Abstractions;
+using Prism.Commands;
+using Prism.Navigation;
+using Prism.Services;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace NotSpotifyApp.ViewModels
 {
-	public class SongPlayerPageViewModel
+	public class SongPlayerPageViewModel : BaseViewModel, IInitialize, INotifyPropertyChanged
 	{
+		public Track TrackInfo { get; set; }
+		public DelegateCommand GetTrackInfoCommand { get; set; }
+		public DelegateCommand PreviousPageCommand { get; set; }
+		public DelegateCommand ShareTrackCommand { get; set; }
+		public event PropertyChangedEventHandler PropertyChanged;
+		public string Id { get; set; }
+
+		public SongPlayerPageViewModel(INavigationService navigationService, IPageDialogService pageDialogueService, IDeezerApiService apiService) : base(navigationService, apiService)
+		{
+			GetTrackInfoCommand = new DelegateCommand(async () =>
+			{
+				await GetTrackData();
+			});
+
+			PreviousPageCommand = new DelegateCommand(async () =>
+			{
+				await navigationService.GoBackAsync();
+			});
+
+			ShareTrackCommand = new DelegateCommand(async () =>
+			{
+				await CrossShare.Current.Share(new ShareMessage
+				{
+					Title = $"Hey check this song out! - {TrackInfo.Title}",
+					Url = $"{TrackInfo.Share}"
+				});
+			});
+		}
+
+		public void Initialize(INavigationParameters parameters)
+		{
+			if (parameters.ContainsKey("Track id"))
+			{
+				Id = parameters["Track id"].ToString();
+				GetTrackInfoCommand.Execute();
+			}
+		}
+
+		async Task GetTrackData()
+		{
+			TrackInfo = await ApiService.GetTrackInfo(Id);
+		}
+
 	}
 }
